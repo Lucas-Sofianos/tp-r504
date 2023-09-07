e864d6e2535c207ad994d2002cae52b56318cc42
Lucas-Sofianos <lucas.sofianos@gmail.com>
2023-09-07 09:56:03 +0200

    actualisation et création d'un fichier

---
+++ TP1/fonctions.py
@@ -0,0 +1,4 @@
+def puissance(a,b):
+	if not type(var) is int:	
+		print(a**b)
+	raise TypeError("Only integers are allowed")
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,14 @@
+import fonctions as f
+
+
 print("Hello World!")
+while True :
+	
+	a=int(input("saisissez un nombre à mettre au Carré : "))
+	b=a**2
+	print("le carré de ",a," est ",b,".")
+	
+	res = f.puissance(a,b)
+	
+	
+	
