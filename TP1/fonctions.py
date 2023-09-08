2f5726662e3b08c65d0a4db2aafbc99070044124
Lucas-Sofianos <lucas.voiry@univ-rouen.fr>
2023-09-08 14:58:59 +0200

    actualisation jusqu'au 4.6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,4 +3,7 @@ def puissance(a,b):
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:	
 		raise TypeError("Only integers are allowed")
+	if a == 0 and b < 0 :
+		raise TypeError("La puissance de zéro n’est pas définie pour des exposants négatifs")
+		
 	return(a**b)
--- TP1/test.py
+++ TP1/test.py
@@ -7,4 +7,10 @@ def test_1 ( ) :
 
 def test_2 ( ) :
 	assert f.puissance( -2 , 3 ) == -8
-	assert f.puissance( -2 , 2 ) == 4
+	assert f.puissance( 2 , -2 ) == 1/(2**2)
+	
+def test_3 ( ) :
+	assert f.puissance( 2 , 0 ) == 1
+	assert f.puissance( 0 , 0 ) == 1
+	
+	
